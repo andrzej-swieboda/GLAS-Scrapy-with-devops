f9c93d5b1537cc82887c76ac0af0a76c44aebfb2
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-15 13:36:03 +0200

    problem with bucket name2

--- TerraformASGL/01-Configuration.tf
+++ TerraformASGL/01-Configuration.tf
@@ -1,7 +1,7 @@
 terraform {
 
   backend "s3" {
-    bucket = "glas-backend-bucket"
+    bucket = "glas-scrapybackend-bucket"
     key    = "production/terraform.tfstate"
     region = "us-east-1"
   }
