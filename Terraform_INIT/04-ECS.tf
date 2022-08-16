21c762aa6f37e3384145281779be7f8ab744582a
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-16 15:26:15 +0200

    ecs work4

--- Terraform_INIT/04-ECS.tf
+++ Terraform_INIT/04-ECS.tf
@@ -16,7 +16,37 @@ resource "aws_ecs_service" "scrapy-dev" {
 
 resource "aws_ecs_task_definition" "service" {
     family  = "service"
-    container_definitions = file("task_definitions/service.json")
-    
+container_definitions = <<TASK_DEFINITION
+[
+  {
+    "cpu": 10,
+    "command": ["sleep", "10"],
+    "entryPoint": ["/"],
+    "environment": [
+      {"name": "VARNAME", "value": "VARVAL"}
+    ],
+    "essential": true,
+    "image": "jenkins",
+    "memory": 128,
+    "name": "jenkins",
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 8080
+      }
+    ],
+        "resourceRequirements":[
+            {
+                "type":"InferenceAccelerator",
+                "value":"device_1"
+            }
+        ]
+  }
+]
+TASK_DEFINITION
 
+  inference_accelerator {
+    device_name = "device_1"
+    device_type = "eia1.medium"
+  }
 }
