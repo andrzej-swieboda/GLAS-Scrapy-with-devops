eb7d29c029671e2e1015e2cd36d63ca4f921d21e
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-16 16:05:27 +0200

    ecs work5 with yaml

--- Terraform_INIT/04-ECS-with terraform.tf
+++ Terraform_INIT/04-ECS-with terraform.tf
@@ -1,12 +1,27 @@
-TASK_DEFINITION
+resource "aws_ecs_cluster" "ecs-dev" {
+  name = "Dev-ecs-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_service" "scrapy-dev" {
+    name            = "scrapy-test-service"
+    cluster         = aws_ecs_cluster.ecs-dev.id
+    task_definition = aws_ecs_task_definition.service.arn
+    desired_count   = 1
+}
+
+resource "aws_ecs_task_definition" "service" {
+    family  = "service"
+container_definitions = <<TASK_DEFINITION
 [
   {
     "cpu": 10,
     "command": ["sleep", "10"],
     "entryPoint": ["/"],
-    "environment": [
-      {"name": "VARNAME", "value": "VARVAL"}
-    ],
     "essential": true,
     "image": "jenkins",
     "memory": 128,
@@ -31,3 +46,4 @@ TASK_DEFINITION
     device_name = "device_1"
     device_type = "eia1.medium"
   }
+}
