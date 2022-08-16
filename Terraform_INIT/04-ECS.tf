12f7f563a43629ff005f352d80c4793fa13dc17f
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-16 15:17:29 +0200

    ecs work3

--- Terraform_INIT/04-ECS.tf
+++ Terraform_INIT/04-ECS.tf
@@ -8,7 +8,15 @@ resource "aws_ecs_cluster" "ecs-dev" {
 }
 
 resource "aws_ecs_service" "scrapy-dev" {
-    name = "scrapy-test-service"
-    cluster = aws_ecs_cluster.ecs-dev.id
-    desired_count = 1
+    name            = "scrapy-test-service"
+    cluster         = aws_ecs_cluster.ecs-dev.id
+    task_definition = aws_ecs_task_definition.service.arn
+    desired_count   = 1
+}
+
+resource "aws_ecs_task_definition" "service" {
+    family  = "service"
+    container_definitions = file("task_definitions/service.json")
+    
+
 }
