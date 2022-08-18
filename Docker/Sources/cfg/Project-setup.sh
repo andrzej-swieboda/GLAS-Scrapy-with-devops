8209a79461cf5f07a8e5addeeeac8651a5bfe8f1
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-18 15:12:54 +0200

    fixing dockerfile11

--- Docker/Sources/cfg/Project-setup.sh
+++ Docker/Sources/cfg/Project-setup.sh
@@ -34,8 +34,6 @@ aws configure set aws_access_key_id AKIAUK3L6UMX44WQJHR7
         aws configure set aws_secret_access_key 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
         aws configure set default.region us-east-1
 
-aws s3 cp -r /outputs/ s3://glas-scrapybackend-dev --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
-
-#aws s3 cp ./$SPIDER_NAME.json s3://glas-scrapybackend-dev --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
+aws s3 cp /outputs/ s3://glas-scrapybackend-dev
 
 
--- Terraform_INIT/01-Configuration.tf
+++ Terraform_INIT/01-Configuration.tf
@@ -1,7 +1,7 @@
 terraform {
 
   backend "s3" {
-    bucket = "glas-scrapybackend-dev"
+    bucket = "scrapy-dev-backend"
     key    = "production/terraform.tfstate"
     region = "us-east-1"
   }
