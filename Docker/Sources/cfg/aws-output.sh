4d6234cb710acfd351e9600c6f1cc770db0c6303
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-18 14:01:21 +0200

    fixing dockerfile4

--- Docker/Sources/cfg/aws-output.sh
+++ Docker/Sources/cfg/aws-output.sh
@@ -1,10 +1,18 @@
 #! /bin/bash
 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
 aws configure
 
-AWS Access Key ID [None]: AKIAUK3L6UMX44WQJHR7
-AWS Secret Access Key [None]: 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
-Default region name [None]: us-east-1
-Default output format [None]: ENTER
+#AWS Access Key ID [None]: 
+AKIAUK3L6UMX44WQJHR7
+#AWS Secret Access Key [None]: 
+i293+l7BS/xExHmcNfjHTDeBqG3Vse1TOwL8O2co
+#Default region name [None]: 
+us-east-1
+#Default output format [None]: 
+ENTER
 
 aws s3 cp /outputs/motywatorka.json s3://glas-scrapybackend-dev --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
