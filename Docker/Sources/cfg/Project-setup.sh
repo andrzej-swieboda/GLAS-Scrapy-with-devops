636042a56866fa46aca7f4bd7baa5196d6ee48ae
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-18 15:21:48 +0200

    fixing dockerfile12

--- Docker/Sources/cfg/Project-setup.sh
+++ Docker/Sources/cfg/Project-setup.sh
@@ -30,10 +30,10 @@ scrapy crawl $SPIDER_NAME -o $SPIDER_NAME.json
 
 cp ./$SPIDER_NAME.json /outputs
 
-aws configure set aws_access_key_id AKIAUK3L6UMX44WQJHR7
-        aws configure set aws_secret_access_key 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
+aws configure set aws_access_key_id AKIA4DZDFY3YTVYMCHUZ
+        aws configure set aws_secret_access_key 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
         aws configure set default.region us-east-1
 
-aws s3 cp /outputs/ s3://glas-scrapybackend-dev
+aws s3 cp /outputs/ s3://scrapy-dev-backends
 
 
