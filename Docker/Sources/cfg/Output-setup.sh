7e9aed7c652e7ba0068b763cb9e4304f2a6e0fa4
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-19 17:24:08 +0200

    fixing prepare for ecs2

--- Docker/Sources/cfg/Output-setup.sh
+++ Docker/Sources/cfg/Output-setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 aws configure set aws_access_key_id $AKID
-    aws configure set aws_secret_access_$SAK
-        aws configure set default.region us-east-1
+aws configure set aws_secret_access_key $SAK
+aws configure set default.region us-east-1
 
 aws s3 cp /outputs/ s3://$BUCKET_NAME --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
