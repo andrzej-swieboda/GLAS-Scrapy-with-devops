e1c1f2df665c3aa068502ac52f01320ee3be306a
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-19 13:39:49 +0200

    fixing dockerfile

--- Docker/Sources/cfg/Project-setup.sh
+++ Docker/Sources/cfg/Project-setup.sh
@@ -3,6 +3,8 @@
 #Set variable for project name
 #only numbers, letters and underscores in names
 
+
+
 set -e
 PROJECT_NAME=MOTI_PROJECT
 SPIDER_NAME=motywatorka
@@ -22,4 +24,9 @@ scrapy crawl $SPIDER_NAME -o $SPIDER_NAME.json
 
 cp ./$SPIDER_NAME.json /outputs
 
+#aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+#        aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+#        aws configure set default.region us-east-1
+
+#aws s3 cp ./$SPIDER_NAME.json s3://glas-scrapybackend-dev --recursive
 
