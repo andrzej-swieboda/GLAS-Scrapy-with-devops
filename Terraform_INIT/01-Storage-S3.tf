bd42f8a7828a3bff1239f798b0f04ed1b158464b
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-22 11:21:29 +0200

    terraform ECS3

--- Terraform_INIT/01-Storage-S3.tf
+++ Terraform_INIT/01-Storage-S3.tf
@@ -1,8 +1,15 @@
 resource "aws_s3_bucket" "output-storage" {
-  bucket = "output-storage"
-  acl    = "private"
+  bucket = "dev-spider-output"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_acl" "output-storage" {
+  bucket = aws_s3_bucket.output-storage.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "output-versioning" {
+  bucket = aws_s3_bucket.output-storage.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
