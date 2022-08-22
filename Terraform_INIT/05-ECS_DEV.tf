635396e678617bc38d3d0ba626edf04c147f5977
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-22 19:11:48 +0200

    ECS10

--- Terraform_INIT/05-ECS_DEV.tf
+++ Terraform_INIT/05-ECS_DEV.tf
@@ -8,6 +8,7 @@ resource "aws_ecs_task_definition" "deploy" {
   family                   = "test"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  executionRoleArn = "arn:aws:iam::693798433817:role/ecsTaskExecutionRole"
   cpu                      = 1024
   memory                   = 2048
   #execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
@@ -44,4 +45,27 @@ resource "aws_iam_role" "ecs_task_role" {
  ]
 }
 EOF
+}
+
+#---------------------ROLE FOR TASK EXECUTION
+
+data "aws_iam_policy_document" "ecs_tasks_execution_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_tasks_execution_role" {
+  name               = "${var.app_env}-ecs-task-execution-role"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_tasks_execution_role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
+  role       = "${aws_iam_role.ecs_tasks_execution_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
