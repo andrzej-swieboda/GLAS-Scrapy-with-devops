23d0c04c442a4fcb121a682569b97489a0883f6b
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-22 15:36:22 +0200

    store prameter implementation

--- Terraform_INIT/01-Storage-S3.tf
+++ Terraform_INIT/01-Storage-S3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "dev-scrapy-output" {
-  bucket = "dev-spider-output"
+  bucket = "dev-scrapy-output"
 }
 
-resource "aws_s3_bucket_versioning" "dev-scrapy-output-versioning" {
+resource "aws_s3_bucket_versioning" "dev-scrapy-output-verioning" {
   bucket = aws_s3_bucket.dev-scrapy-output.id
   versioning_configuration {
     status = "Enabled"
---
+++ Terraform_INIT/Parameter-store.tf
@@ -0,0 +1,23 @@
+resource "aws_ssm_parameter" "AKID" {
+  name        = "AKID"
+  description = "The parameter description"
+  type        = "SecureString"
+  value       = UPDATE_ME
+
+}
+
+resource "aws_ssm_parameter" "SAK" {
+  name        = "SAK"
+  description = "The parameter description"
+  type        = "SecureString"
+  value       = UPDATE_ME
+
+}
+
+resource "aws_ssm_parameter" "OUTPUT_BUCKET_NAME" {
+  name        = "OUTPUT_BUCKET_NAME"
+  description = "The parameter description"
+  type        = "SecureString"
+  value       = dev-scrapy-output
+
+}
