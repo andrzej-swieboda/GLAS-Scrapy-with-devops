31b21f00e0d255f689b9d542334c41b44e0f1a09
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-22 16:24:23 +0200

    store prameter implementation

--- Terraform_INIT/Parameter-store.tf
+++ Terraform_INIT/Parameter-store.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "AKID" {
   name        = "AKID"
   description = "The parameter description"
   type        = "SecureString"
-  value       = UPDATE_ME
+  value       = "UPDATE_ME"
 
 }
 
@@ -10,7 +10,7 @@ resource "aws_ssm_parameter" "SAK" {
   name        = "SAK"
   description = "The parameter description"
   type        = "SecureString"
-  value       = UPDATE_ME
+  value       = "UPDATE_ME"
 
 }
 
@@ -18,6 +18,6 @@ resource "aws_ssm_parameter" "OUTPUT_BUCKET_NAME" {
   name        = "OUTPUT_BUCKET_NAME"
   description = "The parameter description"
   type        = "SecureString"
-  value       = dev-scrapy-output
+  value       = "dev-scrapy-output"
 
 }
