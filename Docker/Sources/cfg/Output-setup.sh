81cc06d3b2a656f6607d7119ab804c3b387975bc
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-23 22:03:06 +0200

    testing area2

--- Docker/Sources/cfg/Output-setup.sh
+++ Docker/Sources/cfg/Output-setup.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+set -e
+PROJECT_NAME=MOTI_PROJECT
+SPIDER_NAME=motywatorka
+SPIDER_FILE=motywatorka_spider.py
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+
 aws configure set aws_access_key_id $AKID
 aws configure set aws_secret_access_key $SAK
 aws configure set default.region us-east-1
