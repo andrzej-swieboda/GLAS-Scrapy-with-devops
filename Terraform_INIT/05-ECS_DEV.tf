c49481835c1d2c795c89414d98387282071727ef
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-23 10:33:06 +0200

    ECS10

--- Terraform_INIT/05-ECS_DEV.tf
+++ Terraform_INIT/05-ECS_DEV.tf
@@ -8,11 +8,10 @@ resource "aws_ecs_task_definition" "deploy" {
   family                   = "test"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  executionRoleArn = "arn:aws:iam::693798433817:role/ecsTaskExecutionRole"
   cpu                      = 1024
   memory                   = 2048
-  #execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  #task_role_arn            = aws_iam_role.ecs_task_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions = <<TASK_DEFINITION
   [
     {
@@ -49,8 +48,12 @@ EOF
 
 #---------------------ROLE FOR TASK EXECUTION
 
-data "aws_iam_policy_document" "ecs_tasks_execution_role" {
+# ECS task execution role data
+data "aws_iam_policy_document" "ecs_task_execution_role" {
+  version = "2012-10-17"
   statement {
+    sid = ""
+    effect = "Allow"
     actions = ["sts:AssumeRole"]
 
     principals {
@@ -60,12 +63,14 @@ data "aws_iam_policy_document" "ecs_tasks_execution_role" {
   }
 }
 
-resource "aws_iam_role" "ecs_tasks_execution_role" {
-  name               = "${var.app_env}-ecs-task-execution-role"
-  assume_role_policy = "${data.aws_iam_policy_document.ecs_tasks_execution_role.json}"
+# ECS task execution role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "dev_ecs_task_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
-  role       = "${aws_iam_role.ecs_tasks_execution_role.name}"
+# ECS task execution role policy attachment
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
