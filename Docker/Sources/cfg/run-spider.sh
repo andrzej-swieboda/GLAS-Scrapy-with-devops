bd573c18aa6f4085acf4977e04dfa4e743bc25c4
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-23 15:56:59 +0200

    renewing bucket info

--- Docker/Sources/cfg/run-spider.sh
+++ Docker/Sources/cfg/run-spider.sh
@@ -18,4 +18,4 @@ aws configure set aws_access_key_id $AKID
     aws configure set aws_secret_access_key $SAK
         aws configure set default.region us-east-1
 
-aws s3 cp /outputs/$SPIDER_NAME$current_time.json s3://$BUCKET_NAME --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
+aws s3 cp /outputs/$SPIDER_NAME$current_time.json s3://$OUTPUT_BUCKET_NAME --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
