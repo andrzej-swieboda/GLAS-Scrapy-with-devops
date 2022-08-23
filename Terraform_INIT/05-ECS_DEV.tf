e74f84cad28a979cab25521ed2be7075dca13325
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-23 12:54:48 +0200

    ECS17

--- Terraform_INIT/05-ECS_DEV.tf
+++ Terraform_INIT/05-ECS_DEV.tf
@@ -73,7 +73,7 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
     sid = ""
     effect = "Allow"
     actions = ["sts:AssumeRole", "ssm:GetParameters"]
-    Resource = "*"
+    resources = "*"
 
     principals {
       type        = "Service"
