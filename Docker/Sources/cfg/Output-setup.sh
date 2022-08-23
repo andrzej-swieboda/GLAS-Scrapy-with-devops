7578723a29bf987fd386367cb32abc406041e849
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-23 23:32:54 +0200

    testing area5

--- Docker/Sources/cfg/Output-setup.sh
+++ Docker/Sources/cfg/Output-setup.sh
@@ -7,7 +7,7 @@ SPIDER_FILE=motywatorka_spider.py
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
-#cd /projects/$PROJECT_NAME/spiders/
+cd /projects/MOTI_PROJECT/spiders/
 
 aws configure set aws_access_key_id $AKID
 aws configure set aws_secret_access_key $SAK
