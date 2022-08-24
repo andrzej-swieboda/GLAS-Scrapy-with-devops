4996673d2a2086fccaab971ebcde33c01c695349
andrzej-swieboda <106965949+andrzej-swieboda@users.noreply.github.com>
2022-08-24 16:29:33 +0200

    Update Project-setup.sh

--- Docker/Sources/cfg/Project-setup.sh
+++ Docker/Sources/cfg/Project-setup.sh
@@ -4,19 +4,25 @@
 #only numbers, letters and underscores in names
 
 
+
 set -e
 PROJECT_NAME=MOTI_PROJECT
 SPIDER_NAME=motywatorka
 SPIDER_FILE=motywatorka_spider.py
 
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+
+mkdir output
+
 scrapy startproject $PROJECT_NAME ../projects
 cp ./projects/$SPIDER_FILE ./projects/$PROJECT_NAME/spiders
 
-echo "Script Completed with project name $PROJECT_NAME"
+cd ./projects/$PROJECT_NAME/spiders/
 
-cd ./projects/$PROJECT_NAME/spiders
+echo "Script Completed with project name $PROJECT_NAME"
+echo "Initiating spider crawl"
+
 scrapy crawl $SPIDER_NAME
 
-echo "Spider crawled"
 
 
