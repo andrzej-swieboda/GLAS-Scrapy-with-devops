73d7cffbe7f09ae48ce132dd5995df31747b9c1b
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-24 10:39:39 +0200

    testing area9

--- Docker/Sources/cfg/Output-setup.sh
+++ Docker/Sources/cfg/Output-setup.sh
@@ -10,9 +10,9 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
 cd /projects/MOTI_PROJECT/spiders/
 
-scrapy crawl motywatorka -o nowy$current_time.json
+scrapy crawl motywatorka -o new-$current_time.json
 
-cp /projects/MOTI_PROJECT/spiders/new*.json /outputs
+cp /projects/MOTI_PROJECT/spiders/new-*.json /outputs
 
 aws configure set aws_access_key_id $AKID
 aws configure set aws_secret_access_key $SAK
