4e7c93b011c1ebabcc9e42aaab472e6a5fd4b138
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-24 13:32:45 +0200

    quickfix4

--- Terraform_INIT/00-Configuration.tf
+++ Terraform_INIT/00-Configuration.tf
@@ -1,7 +1,7 @@
 terraform {
 
   backend "s3" {
-    bucket = "scrapy-dev-backend-bucket"
+    bucket = "scrapy-dev-backend"
     key    = "production/terraform.tfstate"
     region = "us-east-1"
   }
