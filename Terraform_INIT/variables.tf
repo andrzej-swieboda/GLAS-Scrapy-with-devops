6e48a1e5ebc06b9a70fb5f10dd536eb514870009
andrzej.swieboda <andrzej.swieboda@globallogic.com>
2022-08-26 11:50:46 +0200

    preparing environments and tests8

--- Terraform_INIT/variables.tf
+++ Terraform_INIT/variables.tf
@@ -4,7 +4,6 @@ variable "dev_vpc_cidr" {}
 variable "public_subnets" {}
 variable "private_subnets" {}
 variable "app_name" {}
-variable "app_env" {}
 variable "dev_image_tag" {}
 variable "caller_id" {}
 variable "dev_ecr_url" {}
